40f12ec533022c4cc578de7dc7efffa79820bb29
merisantana <86296843+merisantana@users.noreply.github.com>
2021-07-30 09:49:49 +0200

    Create Vars.tf

---
+++ Vars.tf
@@ -0,0 +1,12 @@
+Variable "resourcerm_group_name" {
+  type "string"
+}
+Variable "location" {
+  type "string"
+}
+Variable "name_service_plan" {
+  type "string"
+}
+Variable "name_web_app" {
+  type "string"
+}
